80cc2a47c64a337076e9109bfe2b6efe42f3b72c
Ahmad Zaki <shaikhzaki8@gmail.com>
2022-07-17 02:43:06 +0530

    chakra

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import styles from "./counter.module.css";
+import {Button} from '@chakra-ui/react';
 
 const Counter = ({count}) => {
   // sample value to be replaced
@@ -11,19 +12,21 @@ const Counter = ({count}) => {
 
   return (
     <div className={styles.counter}>
-      <button 
+      <Button
+        colorScheme='cyan'
         data-testid="task-counter-increment-button"
         className={styles.button}
         onClick={() => setCounter(counter + 1)}
         >
           Inc Count
-        </button>
-      <span data-testid="task-counter-value">{counter}</span>
-      <button 
+        </Button>
+      <span data-testid="task-counter-value" className={styles.counter}>{counter}</span>
+      <Button 
+        colorScheme='cyan' 
         data-testid="task-counter-decrement-button"
         className={styles.button}
         onClick={() => counter > 1 && setCounter(counter - 1)}
-        >Dec Count</button>
+        >Dec Count</Button>
     </div>
   );
 };
